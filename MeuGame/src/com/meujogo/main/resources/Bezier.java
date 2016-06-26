df397a3eddbcfa3dce56f96a9a1a36e6b62b64fc
Suporte <Suporte@Suporte-NT-Dell>
2016-06-26 12:47:09 -0400

    esqueleto bezier pronto

--- MeuGame/src/com/meujogo/main/resources/Bezier.java
+++ MeuGame/src/com/meujogo/main/resources/Bezier.java
@@ -1,5 +1,6 @@
 package com.meujogo.main.resources;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
@@ -13,6 +14,7 @@ public class Bezier {
 	private int suave;
 	
 	private List < Point > retaR;
+	private List < Point > retaS;
 	
 	public Bezier(Point inicio, Point controle, Point fim, int pontos) {
 		super();
@@ -22,6 +24,11 @@ public class Bezier {
 		suave = pontos;
 		
 		retaR = new ArrayList<Point> ();
+		retaS = new ArrayList<Point> ();
+		
+		retaR.add(inicio);
+		retaS.add(controle);
+		
 		processa(suave);
 	}
 	
@@ -29,23 +36,36 @@ public class Bezier {
 		int ptx = (int) (inicio.getX()-controle.getX())/pontos;
 		int pty = (int) (inicio.getY()-controle.getY())/pontos;
 		
-		addPontos(Math.abs(ptx), Math.abs(pty));
+		addPontos(retaR, Math.abs(ptx), Math.abs(pty));
+		
+		ptx = (int) (fim.getX()-controle.getX())/pontos;
+		pty = (int) (fim.getY()-controle.getY())/pontos;
+		
+		addPontos(retaS, ptx, pty);
+		///retaS.add(fim);
 	}
 
-	private void addPontos(int ptx, int pty) {
-		for(int i=0; i<suave; i++){
-			retaR.add(new Point(inicio.x+(i*ptx),inicio.y+(i*pty)));
+	private void addPontos(List <Point> reta, int ptx, int pty) {
+		Point ponto =reta.get(0);
+		
+		for(int i=1; i<suave; i++){
+			
+			reta.add(new Point(ponto.x+(i*ptx),ponto.y+(i*pty)));
 		}
 	}
 	
-	
 	public void render(Graphics g){
 		
-		
-		Point point = new Point();
-		for(int i=0; i<retaR.size()-1; i++){
-			point = retaR.get(1);
-			g.drawLine((int)point.getX(), (int)point.getY(),(int) point.getX()+40, (int)point.getX()+40);
+		//g.drawLine(inicio.x, inicio.y, controle.x, controle.y);
+		//g.drawLine(controle.x, controle.y, fim.x, fim.y);
+		for(int i=0; i<retaR.size(); i++){
+			g.setColor(Color.BLUE);
+			g.drawLine((int)retaR.get(i).getX(), (int)retaR.get(i).getY(), (int)retaS.get(i).getX(), (int)retaS.get(i).getY());
+			
+			g.setColor(Color.CYAN);
+			g.drawString("Inicio", inicio.x, inicio.y);
+			g.drawString("Controle", controle.x, controle.y);
+			g.drawString("Fim", fim.x, fim.y);
 		}
 	}
 }
